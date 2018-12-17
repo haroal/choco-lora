3f8462a4e96de92a8ba40788155584e325eb2110
VouRennes <chloe.voury@supelec.fr>
2018-12-17 17:00:50 +0100

    Commit de l'appli avec bluetooth

--- app/Chocolora/App/Containers/LaunchScreen.js
+++ app/Chocolora/App/Containers/LaunchScreen.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
 import { ScrollView, Text, Image, View } from 'react-native'
 import { Images } from '../Themes'
+import { BleManager } from 'react-native-ble-plx'
 
 // Styles
 import styles from './Styles/LaunchScreenStyles'
 
 export default class LaunchScreen extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      btState: null
+    }
+  }
+
+  async componentDidMount() {
+    const bleManager = new BleManager()
+    const state = await bleManager.state()
+
+    this.setState({
+      btState: state
+    })
+  }
+
   render () {
     return (
       <View style={styles.mainContainer}>
@@ -18,6 +36,7 @@ export default class LaunchScreen extends Component {
           <View style={styles.section} >
             <Image source={Images.ready} />
             <Text style={styles.sectionText}>
+              Bluetooth state: {this.state.btState}.
               This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
             </Text>
           </View>
